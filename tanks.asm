706fd36969e8f60bf29fcf2ede0d31fd40a9783b
shehab <shehabalaa97@gmail.com>
2017-11-22 01:28:09 +0200

    input string done

--- tanks.asm
+++ tanks.asm
@@ -2,55 +2,66 @@
 ;00 up
 ;11 down
 ;01 right
-;10 left
-.286
+;10 left  
 include test.inc
 .model medium
 .stack 64d
 .data
-user1 label byte; +24 to reach shoots +0 to reach tank2
-tank1 dw 10d,110d,50d,110d,50d,170d,10d,170d, 20d,125d,40d,125d,40d,155d,20d,155d, 28d,120d,32d,120d,32d,125d,28d,125d ;three rectangles more word for hp and orientation
-shoots1 dw 5d,1d
-dw 10d,?,?
-dw 'ab',?,?
-dw 'cd',?,?
-dw 'ef',?,?
-dw 'gz',?,?
+user1 label byte; +48 to get center + 52 to get orientation and hp word                                                                                                     
+tank1 dw 30d,110d,70d,110d,70d,170d,30d,170d,  40d,125d,60d,125d,60d,155d,40d,155d, 48d,120d,52d,120d,52d,125d,48d,125d, 50d,140d ,0d ;three rectangles 2 words tankcenter  more word for hp and orientation
+shoots1 dw 5d,0d
+dw ?,?,?
+dw ?,?,?
+dw ?,?,?
+dw ?,?,?
+dw ?,?,?
 
-user2 label byte; +24 to reach shoots +0 to reach tank2
-tank2 dw 110d,110d,150d,110d,150d,170d,110d,170d, 120d,125d,140d,125d,140d,155d,120d,155d, 128d,120d,132d,120d,132d,125d,128d,125d
+user2 label byte; +48 to get center + 52 to get orientation and hp word
+tank2 dw 130d,110d,170d,110d,170d,170d,130d,170d, 140d,125d,160d,125d,160d,155d,140d,155d, 148d,120d,152d,120d,152d,125d,148d,125d, 150d,140d ,0d
 shoots2 dw 5d,0d
 dw ?,?,?
 dw ?,?,?
 dw ?,?,?
 dw ?,?,?
-dw ?,?,?
+dw ?,?,?   
+
 mes db 'a $'
+integar db ' '
+
 .code 
-main proc far
+main proc far 
     mov AX,@Data
     mov DS,AX
-    mov es,ax
+    mov es,ax    
+    
     mov ah,0
     mov al,12h
     int 10h   
     mov bx ,offset tank1
-    mov cx ,12d
+    mov cx ,13d
     loop1:
     mov al ,1d
     call drawpx
     add bx,4d
     loop loop1
     mov bx ,offset tank2
-    mov cx ,12d
+    mov cx ,13d
     loop2:
     mov al ,2d
     call drawpx
     add bx,4d
     loop loop2
-    l1:
+    mov ax,2d 
+    mov tank1+52,ax
+    l1: 
+    mov bx,offset shoots1
     call inputshots
+    mov bx, offset shoots1+4
+    call drawpx
+  
+     
     jmp l1
+    
     hlt
 main endp
 
@@ -65,9 +76,10 @@ inputshots proc near
     popa
     ret
 con:
-    mov ah,1
+    xor ax,ax
+    mov ah,1d
     int 16h
-    jz con2
+    jnz con2
     popa
     ret
 con2:
@@ -77,17 +89,56 @@ con2:
     ret
 con3:
     mov ah,0
-    int 16h
-    mov [bx]+2,ax
-    mov bl,6 
-    mul bl
+    int 16h     
+;-----------
+    mov ax,[bx]+2
+    mov cl,6 
+    mul cl
     mov cx,[bx]+2
     inc cx
-    add [bx]+2,cx 
+    mov [bx]+2,cx 
     add bx,4
-    add bx,ax
+    add bx,ax 
+    
+    mov cx  , tank1+48
+    mov [bx], cx
+    mov cx  , tank1+50
+    mov [bx] +2 , cx
+    mov cx  , tank1+52
+    and cl ,00000011b  
+    mov [bx] +4 ,cx
+    
+    cmp cl , 0d
+    jnz cas2
+    mov si,[bx +2]
+    sub si ,35
+    mov [bx+2],si
+    jmp finish
+cas2:
+    cmp cl ,1d
+    jnz cas3
+    mov si,[bx]
+    add si ,35
+    mov [bx],si
+    printstring mes  
+    jmp finish
+cas3:    
+    cmp cl , 2d 
+    jnz cas4
+    mov si,[bx]
+    sub si ,35
+    mov [bx],si 
+    jmp finish
+cas4:    
+    cmp cl , 3d
+    jnz finish
+    mov si,[bx +2]
+    add si ,35
+    mov [bx+2],si
+    
+finish:
 
-    printstring mes
+;---------------    
     popa
     ret
 inputshots endp
