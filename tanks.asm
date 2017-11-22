ec522f9ee566531d1c5f9815c8df86ea5891490f
root <root@localhost.localdomain>
2017-11-22 18:56:04 +0200

    better shots shape

--- tanks.asm
+++ tanks.asm
@@ -3,13 +3,12 @@
 ;11 down
 ;01 right
 ;10 left  
-.286
 include test.inc
 .model medium
 .stack 64d
 .data
 user1 label byte; +48 to get center + 52 to get orientation and hp word                                                                                                     
-tank1 dw 30d,110d,70d,110d,70d,170d,30d,170d,  40d,125d,60d,125d,60d,155d,40d,155d, 48d,120d,52d,120d,52d,125d,48d,125d, 50d,140d ,0d ;three rectangles 2 words tankcenter  more word for hp and orientation
+tank1 dw 30d,110d,70d,110d,70d,170d,30d,170d,  40d,125d,60d,125d,60d,155d,40d,155d, 48d,120d,52d,120d,52d,125d,48d,125d, 50d,140d ,0701h ;three rectangles 2 words tankcenter  more word for hp and orientation
 shots1 dw 5d,0d
 dw ?,?,?
 dw ?,?,?
@@ -26,7 +25,7 @@ dw ?,?,?
 dw ?,?,?
 dw ?,?,?   
 
-mes db 'a $'
+mes dw '  ' ,'$$'
 integar db ' '
 
 .code 
@@ -52,20 +51,19 @@ main proc far
         call drawpx
         add bx,4d
     loop loop2
-        mov ax,0d 
-        mov tank1+52,ax
     l1: 
         mov bx,offset shots1
         call inputshots
         mov bx, offset shots1+4
-        mov al,0ch
-        call drawpx
-        ;call drawshot
-        mov bx, offset shots1
-        call process_shots
-       
-    jmp l1
-    
+        call drawshot
+       ; mov bx, offset shots1
+       ;call process_shots
+        mov ax,tank1 + 52d
+        add ah ,'0'
+        
+        mov mes ,ah
+        PrintString mes 
+    jz l1
     hlt
 main endp
 
@@ -79,22 +77,22 @@ inputshots proc near
     jnz con
     popa
     ret
-con:
+    con:
     xor ax,ax
     mov ah,1d
     int 16h
     jnz con2
     popa
     ret
-con2:
+    con2:
     cmp ah,57d
     jz con3
     popa
     ret
-con3:
+    con3:
     mov ah,0
     int 16h     
-;-----------
+    ;-----------
     mov ax,[bx]+2
     mov cl,6 
     mul cl
@@ -103,7 +101,7 @@ con3:
     mov [bx]+2,cx 
     add bx,4
     add bx,ax 
-    
+    ;--------- 
     mov cx  , tank1+48
     mov [bx], cx
     mov cx  , tank1+50
@@ -111,37 +109,35 @@ con3:
     mov cx  , tank1+52
     and cl ,00000011b  
     mov [bx] +4 ,cx
-cas1:    
-    cmp cl , 0d
-    jnz cas2
-    mov si,[bx +2]
-    sub si ,35
-    mov [bx+2],si
-    jmp finish
-cas2:
-    cmp cl ,1d
-    jnz cas3
-    mov si,[bx]
-    add si ,35
-    mov [bx],si
-    jmp finish
-cas3:    
-    cmp cl , 2d 
-    jnz cas4
-    mov si,[bx]
-    sub si ,35
-    mov [bx],si 
-    jmp finish
-cas4:    
-    cmp cl , 3d
-    jnz finish
-    mov si,[bx +2]
-    add si ,35
-    mov [bx+2],si
-    
-finish:
-
-;---------------    
+    cas1:    
+        cmp cl , 0d
+        jnz cas2
+        mov si,[bx +2]
+        sub si ,35
+        mov [bx+2],si
+        jmp finish
+    cas2:
+        cmp cl ,1d
+        jnz cas3
+        mov si,[bx]
+        add si ,35
+        mov [bx],si
+        jmp finish
+    cas3:    
+        cmp cl , 2d 
+        jnz cas4
+        mov si,[bx]
+        sub si ,35
+        mov [bx],si 
+        jmp finish
+    cas4:    
+        cmp cl , 3d
+        jnz finish
+        mov si,[bx +2]
+        add si ,35
+        mov [bx+2],si
+    finish:
+    ;---------------    
     popa
     ret
 inputshots endp
@@ -154,40 +150,39 @@ process_shots proc near
     popa
     ret
     cont:
-    
     add bx,4
-moveshots:
-    mov ax,[bx]+4
-    cas_1:    
-        cmp al , 0d
-        jnz cas_2
-        mov si,[bx +2]
-        sub si ,2
-        mov [bx+2],si
-        jmp finish2
-    cas_2:
-        cmp al ,1d
-        jnz cas_3
-        mov si,[bx]
-        add si ,2
-        mov [bx],si
-        jmp finish2
-    cas_3:    
-        cmp al , 2d 
-        jnz cas_4
-        mov si,[bx]
-        sub si ,2
-        mov [bx],si 
-        jmp finish2
-    cas_4:    
-        cmp al , 3d
-        jnz finish2
-        mov si,[bx +2]
-        add si ,2d
-        mov [bx+2],si
-    finish2:
-    add bx,6d
-loop moveshots
+    moveshots:
+        mov ax,[bx]+4
+        cas_1:    
+            cmp al , 0d
+            jnz cas_2
+            mov si,[bx +2]
+            sub si ,2
+            mov [bx+2],si
+            jmp finish2
+        cas_2:
+            cmp al ,1d
+            jnz cas_3
+            mov si,[bx]
+            add si ,2
+            mov [bx],si
+            jmp finish2
+        cas_3:    
+            cmp al , 2d 
+            jnz cas_4
+            mov si,[bx]
+            sub si ,2
+            mov [bx],si 
+            jmp finish2
+        cas_4:    
+            cmp al , 3d
+            jnz finish2
+            mov si,[bx +2]
+            add si ,2d
+            mov [bx+2],si
+        finish2:
+        add bx,6d
+    loop moveshots
     popa
     ret
 process_shots endp
@@ -195,37 +190,46 @@ process_shots endp
 
 drawshot proc near
     pusha
+    mov al ,0ch
     mov cx,[bx]
     mov dx,[bx]+2
+    
     mov ah,0ch
     int 10h
-;-----
+    ;-----
     inc cx
     mov ah,0ch
     int 10h
-;-----
+    ;-----
     inc dx
     mov ah,0ch
     int 10h
-;-----
+    ;-----
     dec cx
     mov ah,0ch
     int 10h
-;-----
+    ;-----
     dec cx
     mov ah,0ch
     int 10h
-    popa
-;-----
-    inc dx
+    ;-----
+    dec dx
+    mov ah,0ch
+    int 10h
+    ;-----
+    dec dx
+    mov ah,0ch
+    int 10h
+    ;-----
+    inc cx
+    mov ah,0ch
+    int 10h
+    ;-----
+    inc cx
     mov ah,0ch
     int 10h
     popa
-;-----
-    inc dx
-    mov ah,0ch
-    int 10h
-    popa
+    ret
 drawshot endp
 
 
